f0a102f807d4400379300e4b460e4164caa630bc
Ariel <ariel@comda.co.il>
2022-04-16 23:09:01 +0300

    initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using DatingApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var worker = await _context.Users.FindAsync(id);
+            return worker == null ? NotFound() : Ok(worker);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DatingApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+    }
+}
